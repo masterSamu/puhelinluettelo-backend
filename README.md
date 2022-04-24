97b5350875d4625d7b2977695d7b1af2ecd6c810
Samu <samu.uunonen@student.hamk.fi>
2022-04-24 09:58:15 +0300

    commiting project for github

---
+++ README.md
@@ -0,0 +1,2 @@
+###### Linkki toimivaan backend sovellukseen:  
+https://guarded-temple-50134.herokuapp.com/api/persons
---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('build'))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+app.use(cors());
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<p>This is phonebook app. Watch <a href=/info>info</a></p>");
+})
+
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (body.name.length === 0) {
+    return response.status(400).json({ error: "Name missing" });
+  }
+  if (body.number.length === 0) {
+    return response.status(400).json({ error: "Number missing" });
+  }
+
+  const personExist = persons.find((p) => p.name === body.name);
+  if (personExist) {
+    return response
+      .status(400)
+      .json({ error: `Name '${body.name}' already exists in phonebook` });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 9999),
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
